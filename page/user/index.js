14c6a039cf6c1c34cedae184d5e56ff1c4ecb910
bigsu <792011943@qq.com>
2018-05-20 15:32:40 +0800

    完善我的页面

--- page/user/index.js
+++ page/user/index.js
@@ -3,14 +3,42 @@ Page({
   data: {
     hasUserInfo: false
   },
+  onShow:function(){
+    console.log(app.globalData.userInfo);
+      console.log(app.globalData.hasLogin);
+
+  },
   bindGetUserInfo: function (e) {
     var that = this
     app.globalData.userInfo = e.detail.userInfo,
-    app.globalData.hasLogin = true
+      app.globalData.hasLogin = true
 
     that.setData({
       hasUserInfo: true,
       userInfo: e.detail.userInfo
-    })  
+    })
+  },
+  clear: function () {
+    var that = this;
+    app.globalData.userInfo = null,
+      app.globalData.hasLogin = false
+
+    that.setData({
+      hasUserInfo: false,
+      userInfo: {}
+    })
+    wx.clearStorage();
+
+    wx.showToast({
+      title: '清理完成',
+      icon: 'success',
+      duration: 2000
+    })
+  },
+  getBarcode:function(){
+    wx.previewImage({
+      current: 'http://p8k57dlbo.bkt.clouddn.com/wx_barcode.png', // 当前显示图片的http链接
+      urls: ['http://p8k57dlbo.bkt.clouddn.com/wx_barcode.png'] // 需要预览的图片http链接列表
+    })
   }
 })
