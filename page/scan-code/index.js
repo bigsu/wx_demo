82637b7751683580cc367a517f4d7c972ae4eb7d
bigsu <792011943@qq.com>
2018-09-10 09:07:25 +0800

    完成二维码生成

--- page/scan-code/index.js
+++ page/scan-code/index.js
@@ -3,46 +3,60 @@ import drawQrcode from '../../util/qrcode.js'
 Page({
   data: {
     result: '',
-    inputValue:''
+    inputValue: ''
   },
-  bindKeyInput: function (e) {
+  bindKeyInput: function(e) {
     this.setData({
       inputValue: e.detail.value
     })
   },
-  getcode: function (e) {
-   drawQrcode({
+  getcode: function(e) {
+    drawQrcode({
       width: 200,
       height: 200,
       canvasId: 'myQrcode',
-      typeNumber: 10,
-      text: e.target.dataset.code,
-      callback(e) {
-        console.log('e: ', e)
+      // typeNumber: 10,
+      text: e.target.dataset.code
+    })
+    wx.canvasToTempFilePath({　　　　
+      canvasId: 'myQrcode', //通过id 指定是哪个canvas
+      success(res) {　
+        //成功之后保存到本地
+        wx.saveImageToPhotosAlbum({
+          filePath: res.tempFilePath,
+          success: function(res) {
+            wx.showToast({
+              title: '已保存到相册',
+              icon: 'success',
+              duration: 2000
+            })
+          },
+          fail: function(res) {
+            // console.log(res)
+          }
+        })
       }
-    })   
+    })
   },
 
-
-  onHide: function () {
+  onHide: function() {
     var that = this
     that.setData({
       result: ''
     })
   },
-  scanCode: function () {
+  scanCode: function() {
     var that = this
     wx.scanCode({
-      success: function (res) {
+      success: function(res) {
         that.setData({
           result: res.result
         })
       },
-      fail: function (res) {
-      }
+      fail: function(res) {}
     })
   },
-  copy: function (e) {
+  copy: function(e) {
     if (e.currentTarget.dataset.text.length > 0) {
       var that = this
       that.setData({
